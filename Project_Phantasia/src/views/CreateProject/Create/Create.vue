d34f938fa23b78064cccf64788b0b90a02e81254
您的姓名 <ling0967253511@email.com>
2025-02-17 03:09:09 +0800

    createrenew

--- Project_Phantasia/src/views/CreateProject/Create/Create.vue
+++ Project_Phantasia/src/views/CreateProject/Create/Create.vue
@@ -4,49 +4,53 @@
 
 <template>
   <div class="Create">
-    <!-- 左側邊欄 -->
-    <div class="sidebar left-sidebar" :class="{ collapsed: isLeftCollapsed }">
-      <button @click="toggleLeftSidebar">
-        {{ isLeftCollapsed ? "展開" : "收縮" }}
-      </button>
-      <div class="sidebar-content">
-        <!-- 左側邊欄內容 -->
-        左側邊欄內容
+    <!-- SideBarGroups -->
+    <div class="CreateMenu">
+      <!-- 左邊-Function  -->
+      <div class="Menu Function" :class="{ close: isLeftClose }">
+        <div class="FunctionBar">
+          <CheckBox></CheckBox>
+          <span class="Toggle" :class="{ close: isLeftClose }" @click="toggleFunction" />
+        </div>
+        <div class="MenuContent" :class="{ close: isLeftClose }">
+          <CreateMenuContent />
+        </div>
+      </div>
+
+      <!-- 右側-PageView -->
+      <div class="Menu PageView" :class="{ close: isRightClose }">
+        <div class="topBar">
+          <span class="Toggle" :class="{ close: isRightClose }" @click="togglePageView" />
+        </div>
+        <div class="MenuContent" :class="{ close: isRightClose }">
+          <!-- 右側邊欄內容 -->
+          右側邊欄內容
+        </div>
       </div>
     </div>
-
     <!-- 主要內容區域 -->
     <div class="main-content">主要內容區域</div>
-
-    <!-- 右側邊欄 -->
-    <div class="sidebar right-sidebar" :class="{ collapsed: isRightCollapsed }">
-      <button @click="toggleRightSidebar">
-        {{ isRightCollapsed ? "展開" : "收縮" }}
-      </button>
-      <div class="sidebar-content">
-        <!-- 右側邊欄內容 -->
-        右側邊欄內容
-      </div>
-    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import CheckBox from "../../../components/Input/CheckBox.vue";
+import CreateMenuContent from "../../../components/CreateMenuContent.vue";
 
 // 左側邊欄的收縮狀態
-const isLeftCollapsed = ref(false);
+const isLeftClose = ref(false);
 
 // 右側邊欄的收縮狀態
-const isRightCollapsed = ref(false);
+const isRightClose = ref(false);
 
 // 切換左側邊欄的收縮狀態
-const toggleLeftSidebar = () => {
-  isLeftCollapsed.value = !isLeftCollapsed.value;
+const toggleFunction = () => {
+  isLeftClose.value = !isLeftClose.value;
 };
 
 // 切換右側邊欄的收縮狀態
-const toggleRightSidebar = () => {
-  isRightCollapsed.value = !isRightCollapsed.value;
+const togglePageView = () => {
+  isRightClose.value = !isRightClose.value;
 };
 </script>
